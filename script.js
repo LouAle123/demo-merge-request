1b555811c7efd6e04e90cee6537ebde87abe8580
Lourdes <lourdesoliva98@gmail.com>
2025-05-28 20:56:31 -0300

    Agregado nombre Lourdes Oliva

---
+++ script.js
@@ -0,0 +1,7 @@
+// 🚀 Agregá tu nombre acá como una constante
+// Ejemplo:
+const alumno = "Brenda";
+console.log("Participante:", alumno);
+
+const alumno2 = "Lourdes Oliva";
+console.log("Participante:", alumno2);
